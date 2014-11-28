b2495e7012d659368ea9b96ca1ff5d1043fce2db
Kiddi <kiddinn95@gmail.com>
2014-11-28 11:21:54 +0000

    Velkomin.txt

--- vip.h
+++ vip.h
@@ -1,10 +1,15 @@
 #ifndef VIP_H
 #define VIP_H
-
+#include <string>
 class VIP
 {
 public:
     VIP();
+private:
+    string name;
+    int gender;
+    int byear;
+    int dyear;
 };
 
 #endif // VIP_H
